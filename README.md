fa9a01e3fb2b0216be8070670bacf15dddc4b218
enzo filippo <enzo.filippo23@gmail.com>
2025-07-10 13:32:29 +1000

    added drizzle config

--- README.md
+++ README.md
@@ -29,3 +29,7 @@
 With the neon-http and neon-websockets drivers, you can access a Neon database from serverless environments over HTTP or WebSockets instead of TCP. Querying over HTTP is faster for single, non-interactive transactions.
 
 If you need session or interactive transaction support, or a fully compatible drop-in replacement for the pg driver, you can use the WebSocket-based neon-serverless driver. You can connect to a Neon database directly using Postgres
+
+## Drizzle
+
+- Apply changes to the database using "drizzle-kit push" command.
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,7 @@
-import { neon } from "@neondatabase/serverless";
+// import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import { eq } from "drizzle-orm";
+import { usersTable } from "./schema";
 
 const DB_URL = process.env.DATABASE_URL;
 
@@ -6,7 +9,42 @@ if (!DB_URL) {
   throw new Error("DATABASE_URL environment variable is not set");
 }
 
-const sql = neon(DB_URL);
+const db = drizzle(DB_URL);
+
+async function main() {
+  const user: typeof usersTable.$inferInsert = {
+    name: "John",
+    age: 30,
+    email: "john@example.com",
+  };
+  await db.insert(usersTable).values(user);
+  console.log("New user created!");
+  const users = await db.select().from(usersTable);
+  console.log("Getting all users from the database: ", users);
+  /*
+  const users: {
+    id: number;
+    name: string;
+    age: number;
+    email: string;
+  }[]
+  */
+  await db
+    .update(usersTable)
+    .set({
+      age: 31,
+    })
+    .where(eq(usersTable.email, user.email));
+  console.log("User info updated!");
+  await db.delete(usersTable).where(eq(usersTable.email, user.email));
+  console.log("User deleted!");
+}
+main();
+
+/** For synchronous connection using Neon **/
+
+// const sql = neon(DB_URL);
+// const db = drizzle({ client: sql })
 
 /* Get Data from a Server Component at runtime */
 
